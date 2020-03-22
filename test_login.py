1628a352914f14c8956307b081488886dc95a6c9
n5g <n3g@mail.ru>
2020-03-22 16:36:43 +0500

    step3
    make login_test simple

--- test_login.py
+++ test_login.py
@@ -2,12 +2,19 @@ from selenium import webdriver
 from selenium.common.exceptions import *
 from selenium_fixture import driver
 
-def test_login(driver):
-    driver.get("http://localhost/php4dvd/")
+def logout(driver):
+    driver.find_element_by_link_text("Log out").click()
+    driver.switch_to.alert.accept()
+
+def login(driver):
     driver.find_element_by_id("username").clear()
     driver.find_element_by_id("username").send_keys("admin")
     driver.find_element_by_name("password").clear()
     driver.find_element_by_name("password").send_keys("admin")
     driver.find_element_by_name("submit").click()
-    driver.find_element_by_link_text("Log out").click()
-    driver.switch_to.alert.accept()
+
+def test_login(driver):
+    driver.get("http://localhost/php4dvd/")
+    login(driver)
+    logout(driver)
+   
