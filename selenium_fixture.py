88a07a2accc94e1eac59da82662833d67a019bdf
n5g <n3g@mail.ru>
2020-03-22 17:02:26 +0500

    create application.py
    создали application метод который хранит ссылку на драйвер, в тестах добавили ссылку на app и изменили фикстуру

--- selenium_fixture.py
+++ selenium_fixture.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
 import pytest
+from model.application import Application
 
 @pytest.fixture
-def driver(request):
+def app(request):
     driver = webdriver.Chrome()
     driver.implicitly_wait(10)
     request.addfinalizer(driver.quit)
-    return driver
+    return Application(driver)
 
--- test_login.py
+++ test_login.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.common.exceptions import *
-from selenium_fixture import driver
+from selenium_fixture import app
 from model.user import User
 
 def logout(driver):
@@ -14,8 +14,8 @@ def login(driver, user):
     driver.find_element_by_name("password").send_keys(user.password)
     driver.find_element_by_name("submit").click()
 
-def test_login(driver):
-    driver.get("http://localhost/php4dvd/")
-    login(driver, User.Admin())
-    logout(driver)
+def test_login(app):
+    app.driver.get("http://localhost/php4dvd/")
+    login(app.driver, User.Admin())
+    logout(app.driver)
 
