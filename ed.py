4530fd8ec10f8c4ca650132619ecf51559633fa0
n5g <55139148+n5g@users.noreply.github.com>
2020-03-26 21:28:01 +0500

    Update ed.py

--- ed.py
+++ ed.py
@@ -1,7 +1,8 @@
 import selenium
 from selenium import webdriver
 import time
-
+ 
+  
 driver = webdriver.Chrome()
 driver.get("http://localhost/php4dvd/")
 driver.find_element_by_id("username").clear()
